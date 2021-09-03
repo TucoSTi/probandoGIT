fd15d8d19a6f42b7d8246097f894385d7a550178
--global <you@example.com>
2021-09-03 14:11:52 -0300

    Agrega el producto

--- operaciones.php
+++ operaciones.php
@@ -3,6 +3,7 @@ $n1 = $_GET['n1'];
 $n2 = $_GET['n2'];
 
 $suma = $n1 + $n2;
+$multi = $n1 * $n2;
 ?> <!DOCTYPE html>
 <html>
 <head>
@@ -14,5 +15,6 @@ link
 <body>
         <h1> Resultado </h1>
 		<?php echo "<p>La suma de $n1 + $n2 = $suma</p>"; ?>
+		<?php echo "<p>El producto entre $n1 * $n2 = $multi</p>"; ?>
 </body>
 </html>
