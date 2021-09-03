32a92e7be8c46d5faad90f858604145cd0f7a0c1
TucoSTi <you@example.com>
2021-09-03 15:33:16 -0300

    Agrega division y evita division por 0

--- operaciones.php
+++ operaciones.php
@@ -5,6 +5,11 @@ $n2 = $_GET['n2'];
 $suma = $n1 + $n2;
 $resta = $n1 - $n2;
 $multi = $n1 * $n2;
+if ($n2 == 0) {
+	$divi = "No se puede dividir por 0";
+} else {
+$divi = $n1 / $n2;
+}
 ?> <!DOCTYPE html>
 <html>
 <head>
@@ -18,5 +23,6 @@ link
 		<?php echo "<p>La suma de $n1 + $n2 = $suma</p>"; ?>
 		<?php echo "<p>El producto entre $n1 * $n2 = $multi</p>"; ?>
 		<?php echo "<p>La resta de $n1 - $n2 = $resta</p>"; ?>
+		<?php echo "<p>La division entre $n1 / $n2 = $divi</p>"; ?>
 </body>
 </html>
